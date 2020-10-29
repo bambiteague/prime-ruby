33e2f94de8ed019f4dae91f5f070bcdaea810459
bambi teague <teeegsa@gmail.com>
2020-10-28 22:19:14 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+
+# define a method 'prime?'
+#    this method will take in an interger arguement and
+#    return a boolean of true or false if the interger is a prime number
+# create a range of numbers and convert the range into an array to be iterated over
+
+def prime?(i)
+    num = 2
+    while i < num 
+        return false if i % num == 0
+    end
+    true
+  
+end
